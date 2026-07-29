01aa426c5b044e4680586513f9786b3f11f8f8b4
Ester Wensink <ester.wensink@proton.me>
2026-07-29 13:59:26 -0400

    Email new users their login credentials when an admin creates their account

--- api/admin/users.js
+++ api/admin/users.js
@@ -1,5 +1,6 @@
 const { getPool } = require('../_lib/db');
 const { ensureAuthTables, getSessionUser, createUser } = require('../_lib/auth');
+const { sendEmail } = require('../_lib/email');
 
 module.exports = async (req, res) => {
   const client = await getPool().connect();
@@ -33,6 +34,23 @@ module.exports = async (req, res) => {
         return;
       }
       const user = await createUser(client, { email, password, name, role });
+
+      try {
+        await sendEmail({
+          to: user.email,
+          subject: 'Your login for the PT routine app',
+          text:
+            `Hi ${name},\n\n` +
+            `An account has been created for you on the PT routine app.\n\n` +
+            `Log in here: https://www.zevenstip.com/login/\n` +
+            `Email: ${user.email}\n` +
+            `Password: ${password}\n\n` +
+            `You can change your password by asking an admin to reset it.`,
+        });
+      } catch (emailErr) {
+        console.error('Failed to send welcome email', emailErr);
+      }
+
       res.status(201).json(user);
       return;
     }
